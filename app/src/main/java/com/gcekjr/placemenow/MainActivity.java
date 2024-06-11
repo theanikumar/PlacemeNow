8c7e9683561a348125e6c6929f53878170c40b4a
Aniket Kumar <91782194+theanikumar@users.noreply.github.com>
2024-06-11 10:12:53 +0530

    1st Comit
    uploading the bottom navigation

---
+++ app/src/main/java/com/gcekjr/placemenow/MainActivity.java
@@ -0,0 +1,22 @@
+package com.gcekjr.placemenow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout frameLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavView);
+        frameLayout = findViewById(R.id.frameLayout);
+    }
+}
